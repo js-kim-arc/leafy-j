e4104d71ef5d0d7b9bbe499d36b792907911a2a3
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-09-03 20:46:20 +0900

    first commit

---
+++ src/test/java/com/example/leafy_j/LeafyJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leafy_j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafyJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
